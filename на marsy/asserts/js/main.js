4242c6d44169f9d39989804d146334ac4981b6fe
Radost0n <pathlogist1@gmail.com>
2024-09-23 20:10:03 +1000

    Update main.js

--- на marsy/asserts/js/main.js
+++ на marsy/asserts/js/main.js
@@ -1,16 +1,11 @@
 $(document).ready(function() {
     $('nav a[href^="#"]').click(function(event) {
-        event.preventDefault(); // Отменяем стандартное поведение ссылки
-
-        // Получаем значение href ссылки
+        event.preventDefault();
         let target = $(this).attr('href');
-        
-        // Анимируем прокрутку страницы к нужному месту
         $('html, body').animate({
             scrollTop: $(target).offset().top
         }, 500);
-
-        return false; // Предотвращаем дальнейшую обработку клика
+        return false;
     });
 });
 
